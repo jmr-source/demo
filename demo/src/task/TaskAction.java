41f4cab4bc9ca550a71b22c9cb830feaecebdbf3
jmr-source <jmr_source@163.com>
2016-07-26 15:44:24 +0800

    new demos

---
+++ demo/src/task/TaskAction.java
@@ -0,0 +1,12 @@
+package task;
+
+import org.eclipse.jet.JET2Context;
+import org.jmr.core.JmrActionSupport;
+
+public class TaskAction extends JmrActionSupport {
+
+	public void execute(JET2Context context) {
+
+	}
+
+}
